d9d0c368fdd2cd51551fe1d2d4536fa5b11592c5
Alvaro Hurtado <alvarohurtado84@gmail.com>
2021-03-31 14:57:40 +0200

    Fix installRuby.sh

--- scripts/codedeploy/installRuby.sh
+++ scripts/codedeploy/installRuby.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
 
 echo 'Install Rbenv'
-curl -fsSL https://github.com/rbenv/rbenv-installer/raw/master/bin/rbenv-installer | bash
+apt install rbenv ruby-build
 
-echo 'Install ruby-build'
-sudo apt-get install ruby-build
-
-echo 'Update Rbenv ruby-build list'
+echo 'Update Rbenv and ruby-build'
 cd
+rm -rf ~/.rbenv
 git clone https://github.com/rbenv/rbenv.git ~/.rbenv
 echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bashrc
 echo 'eval "$(rbenv init -)"' >> ~/.bashrc
 exec $SHELL
+
+rm -rf ~/.rbenv/plugins/ruby-build
 git clone https://github.com/rbenv/ruby-build.git ~/.rbenv/plugins/ruby-build
 echo 'export PATH="$HOME/.rbenv/plugins/ruby-build/bin:$PATH"' >> ~/.bashrc
 exec $SHELL
 
 echo 'Install Ruby'
-rbenv install $(cat /webapps/myApp/.ruby-version)
+rbenv install -v -f $(cat /webapps/myApp/.ruby-version)
 
 echo 'Set as global'
 rbenv global $(cat /webapps/myApp/.ruby-version)
