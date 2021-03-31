3a19387f9326cf0a5c14f19ac27c2ed090031fd6
Alvaro Hurtado <alvarohurtado84@gmail.com>
2021-03-31 14:12:27 +0200

    Added explicit install of ruby-build

--- scripts/codedeploy/installRuby.sh
+++ scripts/codedeploy/installRuby.sh
@@ -3,6 +3,9 @@
 echo 'Install Rbenv'
 curl -fsSL https://github.com/rbenv/rbenv-installer/raw/master/bin/rbenv-installer | bash
 
+echo 'Install ruby-build'
+sudo apt-get install ruby-build
+
 echo 'Update Rbenv ruby-build list'
 cd
 git clone https://github.com/rbenv/rbenv.git ~/.rbenv
