b41fa0c2e99c9bdc9efa9b91b3ed814392733ed1
Alvaro Hurtado <alvarohurtado84@gmail.com>
2021-03-31 11:04:25 +0200

    Install NodeJS + Yarn + Update rbenv build list

--- scripts/codedeploy/installRuby.sh
+++ scripts/codedeploy/installRuby.sh
@@ -3,8 +3,21 @@
 echo 'Install Rbenv'
 sudo apt install rbenv
 
-echo 'Install Ruby'
-rbenv install $(cat .ruby-version)
+echo 'Update Rbenv ruby-build list'
+cd
+git clone https://github.com/rbenv/rbenv.git ~/.rbenv
+echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bashrc
+echo 'eval "$(rbenv init -)"' >> ~/.bashrc
+exec $SHELL
+git clone https://github.com/rbenv/ruby-build.git ~/.rbenv/plugins/ruby-build
+echo 'export PATH="$HOME/.rbenv/plugins/ruby-build/bin:$PATH"' >> ~/.bashrc
+exec $SHELL
 
-echo 'Test Rbenve + Ruby'
+echo 'Install Ruby'
+rbenv install $(cat /webapps/myApp/.ruby-version)
+
+echo 'Set as global'
+rbenv global $(cat /webapps/myApp/.ruby-version)
+
+echo 'Test Rbenv + Ruby'
 curl -fsSL https://github.com/rbenv/rbenv-installer/raw/master/bin/rbenv-doctor | bash
