3590feb96315c21e45bae31f9cd31be3a79f6b14
Alvaro Hurtado <alvarohurtado84@gmail.com>
2021-04-02 12:40:10 +0200

    Finish installing dependencies

--- scripts/codedeploy/installForem.sh
+++ scripts/codedeploy/installForem.sh
@@ -12,11 +12,23 @@ cd /webapps/myApp
 #echo '1. Installing exceptions'
 #sudo gem install racc -v '1.5.2' --source 'https://rubygems.org/'
 
+# ruby dependencies creating problems:
+echo 'Installing unf_ext...'
+sudo gem install unf_ext
+
+echo 'Installing sassc...'
+sudo gem install sassc
+
 # install all ruby dependencies
-bundler install
+echo 'Installing the rest of the gemfile...'
+sudo bundle install
 
 # install foreman
-gem list \"^foreman$\" -v 0.87.1 -i --silent || gem install foreman
+echo 'Install foreman'
+sudo gem install foreman -v 0.87.1
+#gem list \"^foreman$\" -v 0.87.1 -i --silent || gem install foreman
 
 # install javascript dependencies
+# (not sure if this is working fine)
+echo 'Execute bin/yarn'
 bin/yarn
